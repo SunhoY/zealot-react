df55b6994b27c896b759c81d5c530e13eea6f441
Harry Yang <zillaci@gmail.com>
2017-03-06 19:00:47 +0900

    lower title text on Landing Page.

--- src/component/Landing.js
+++ src/component/Landing.js
@@ -8,7 +8,7 @@ const styles = {
     container: {
         width: "100%",
         height: "100vh",
-        paddingTop: "100px"
+        paddingTop: "4vh"
     },
     title: {
         width: "100%",
